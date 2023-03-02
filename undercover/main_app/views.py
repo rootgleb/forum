9ea81a44238894511e86c5d776596ad3e2675591
gleb12321 <blinpavlin3@gmail.com>
2023-03-02 21:44:59 +0300

    v3

--- undercover/main_app/views.py
+++ undercover/main_app/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-
-
 
 
 def index(request):
     posts = reversed(Post.objects.order_by('date'))
     print(posts)
     return render(request, "main_app/index.html", {"posts": posts})
+
+def topic(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'main_app/topic.html', {'post': post})
+
--- undercover/undercover/urls.py
+++ undercover/undercover/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("main_app.urls"), name="home")
+    path('', include("main_app.urls"), name="home"),
 ]
